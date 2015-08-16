146d2382d3ca6595d0534e93b6571fc6d7e5f2e0
Leo Balduf <balduf@hm.edu>
2015-08-16 13:18:57 +0200

    Corrected contact user_id type

--- model/contact.go
+++ model/contact.go
@@ -4,5 +4,5 @@ type Contact struct {
 	PhoneNumber string `json:"phone_number"`
 	FirstName   string `json:"first_name"`
 	LastName    string `json:"last_name"`
-	Id          string `json:"user_id"`
+	Id          int    `json:"user_id"`
 }
