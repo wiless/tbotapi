06d3ce9550c0014eb111c0b2ecd0faa0e76541cc
Leo Balduf <balduf@hm.edu>
2015-08-16 13:29:23 +0200

    Updated outgoingAudio with optional parameters

--- model/outgoingAudio.go
+++ model/outgoingAudio.go
@@ -6,6 +6,12 @@ import (
 
 type OutgoingAudio struct {
 	OutgoingBase
+	duration     int
+	performer    string
+	title        string
+	durationSet  bool
+	performerSet bool
+	titleSet     bool
 }
 
 func NewOutgoingAudio(chatId int) *OutgoingAudio {
@@ -16,8 +22,38 @@ func NewOutgoingAudio(chatId int) *OutgoingAudio {
 	}
 }
 
+func (oa *OutgoingAudio) SetDuration(to int) *OutgoingAudio {
+	oa.duration = to
+	oa.durationSet = true
+	return oa
+}
+
+func (oa *OutgoingAudio) SetPerformer(to string) *OutgoingAudio {
+	oa.performer = to
+	oa.performerSet = true
+	return oa
+}
+
+func (oa *OutgoingAudio) SetTitle(to string) *OutgoingAudio {
+	oa.title = to
+	oa.titleSet = true
+	return oa
+}
+
 func (oa *OutgoingAudio) GetQueryString() Querystring {
 	toReturn := url.Values(oa.GetBaseQueryString())
+
+	if oa.durationSet {
+		toReturn.Set("duration", oa.duration)
+	}
+
+	if oa.performerSet {
+		toReturn.Set("performer", oa.performer)
+	}
+
+	if oa.titleSet {
+		toReturn.Set("title", oa.title)
+	}
 
 	return Querystring(toReturn)
 }
