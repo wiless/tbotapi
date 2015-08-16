27449cfba3a7d3ebf9697a6028b87692a4d2ce5c
Leo Balduf <balduf@hm.edu>
2015-08-16 13:19:16 +0200

    Added optional caption and duration parameters for sendVideo

--- model/outgoingVideo.go
+++ model/outgoingVideo.go
@@ -6,6 +6,10 @@ import (
 
 type OutgoingVideo struct {
 	OutgoingBase
+	duration    int
+	durationSet bool
+	caption     string
+	captionSet  bool
 }
 
 func NewOutgoingVideo(chatId int) *OutgoingVideo {
@@ -16,8 +20,28 @@ func NewOutgoingVideo(chatId int) *OutgoingVideo {
 	}
 }
 
+func (ov *OutgoingVideo) SetCaption(to string) *OutgoingVideo {
+	ov.caption = to
+	ov.captionSet = true
+	return ov
+}
+
+func (ov *OutgoingVideo) SetDuration(to int) *OutgoingVideo {
+	ov.duration = to
+	ov.durationSet = true
+	return ov
+}
+
 func (ov *OutgoingVideo) GetQueryString() Querystring {
 	toReturn := url.Values(ov.GetBaseQueryString())
+
+	if ov.captionSet {
+		toReturn.Set("caption", ov.caption)
+	}
+
+	if ov.durationSet {
+		toReturn.Set("duration", ov.duration)
+	}
 
 	return Querystring(toReturn)
 }
