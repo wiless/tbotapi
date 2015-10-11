9746544d189e1642b1ae7bb1f2244b53f7d25498
Leo Balduf <balduf@hm.edu>
2015-10-11 16:38:03 +0200

    cleaned up

--- model/ReplyMarkup.go
+++ model/ReplyMarkup.go
@@ -7,18 +7,3 @@ type ReplyMarkup interface {
 func (ReplyKeyboardHide) replyMarkup()   {}
 func (ReplyKeyboardMarkup) replyMarkup() {}
 func (ForceReply) replyMarkup()          {}
-
-/*func (r ReplyMarkup) MarshalJSON() ([]byte, error) {
-	t := interface{}(r)
-
-	switch t := t.(type) {
-	case ReplyKeyboardMarkup:
-		return json.Marshal(t)
-	case ReplyKeyboardHide:
-		return json.Marshal(t)
-	case ForceReply:
-		return json.Marshal(t)
-	default:
-		panic("tbotapi: ReplyMarkup must be of type ReplyKeyboardMarkup, ReplyKeyboardHide or ForceReply!")
-	}
-}*/
