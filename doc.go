0fba399e2063d132be3304d2e7666f17ad5a61f7
Leo Balduf <balduf@hm.edu>
2015-10-11 15:05:59 +0200

    Updated package documentation

--- doc.go
+++ doc.go
@@ -3,7 +3,7 @@
 // Note that, if the REST API returns an error, that error will be wrapped in a Go error and returned by the function call.
 // This means that you will never have to examine the Ok value of responses, as the functions do that for you.
 //
-// We currently only support long polling (i.e. no webhooks). Feature-wise, everything up to and including the August 15
+// We currently only support long polling (i.e. no webhooks). Feature-wise, everything up to and including the October 8
 // changes should be implemented.
 //
 // An example bot is implemented in cmd/example.go, so check that out.
